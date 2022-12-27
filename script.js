a546fa2e77f3c680879020f7a74f0314027c7a85
Oleg Maizer <zippooleg3@gmail.com>
2022-12-27 14:45:44 +0200

    Made add new column logic

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let appData = {
                 {
                     title: 'Todo',
                     id: 'c1',
-                    color: '#FFB6C1',
+                    color: '#ff3f3f',
                     cards: [
                         {
                             title: 'first',
@@ -56,7 +56,7 @@ let appData = {
                 {
                     title: 'Todo2',
                     id: 'c5',
-                    color: '#FCE5CD',
+                    color: '#ff3f3f',
                     cards: [
                         {
                             title: 'first',
@@ -75,7 +75,7 @@ let appData = {
                 {
                     title: 'Doing2',
                     id: 'c6',
-                    color: '#D9EAD3',
+                    color: '#FFA500',
                     cards: [
                         {
                             title: 'first second col board2',
@@ -113,8 +113,8 @@ function init() {
     data = JSON.parse(localStorage.getItem('app_data'))
     appData = data
     fillData(data)
-    let currentBoard = getBoard(data.currentBoard)
-    var drake = dragula(currentBoard.cols.map(col => document.getElementById(col.title), { revertOnSpill: true }));
+    let cols = getCols()
+    var drake = dragula(cols.map(col => document.getElementById(col.title), { revertOnSpill: true }));
 }
 
 document.body.onload = () => init()
@@ -134,10 +134,9 @@ const fillData = (data) => {
             boardSelection.textContent = board.title
             BoardSideBar.append(boardSelection)
     })
-    let currentBoard = getBoard(data.currentBoard)
-    console.log(currentBoard)
+    let cols = getCols()
     // create column
-    currentBoard.cols.map(column => {
+    cols.map(column => {
         // create column container
         let columnSection = document.createElement('section')
         columnSection.classList.add('w-72', 'h-full')
@@ -152,7 +151,7 @@ const fillData = (data) => {
         columnTitleSpan.textContent = column.title + ' ' + '(4)'
         // create card container
         let cardContainer = document.createElement('div')
-        cardContainer.classList.add('w-72', 'pt-5', 'space-y-4')
+        cardContainer.classList.add('w-72', 'pt-5', 'space-y-4', 'min-h-[108px]')
         cardContainer.setAttribute('id', column.title)
         // append the column elements to the board container
         columnTitle.append(columnTitleDot, columnTitleSpan)
@@ -199,17 +198,18 @@ const generateId = (type) => {
     return `${type}-${nextId++}`;
 }
 
+
 const addNewTaskContainer = document.querySelector('#add-task-container')
-const addNewTaskBackground = document.querySelector('#add-task-background')
 const addNewTaskBtn = document.querySelector('#new-task-btn')
 const statusSelect = document.querySelector('#status') 
 
 const showAddNewTask = () => {
     addNewTaskBackground.removeAttribute('hidden')
     addNewTaskContainer.removeAttribute('hidden')
-    board = getBoard(appData.currentBoard)
+    let cols = getCols()
     statusSelect.innerHTML = ''
-    board.cols.map(col => {
+    taskContainer.innerHTML = ''
+    cols.map(col => {
         let optionEl = document.createElement('option')
             optionEl.setAttribute('value', col.title)
             optionEl.textContent = col.title
@@ -218,52 +218,79 @@ const showAddNewTask = () => {
 }
 addNewTaskBtn.addEventListener('click', () =>showAddNewTask())
 
-const hideAddNewTask = () => {
-    addNewTaskBackground.setAttribute('hidden',true)
-    addNewTaskContainer.setAttribute('hidden',true)
-    form.reset();
-}
-addNewTaskBackground.addEventListener('click', (e) => {
-    console.log(e)
-    if(e.target.id === addNewTaskBackground.id) hideAddNewTask();
-})
 
-const getRandomPastelColor = () => {
-    const pastelColors = [
-        '#FFB6C1', '#FFC0CB', '#F4D03F', '#F5B183', '#F5F5DC',
-        '#F8D7D4', '#F9E79F', '#F9EBEA', '#FAE5D3', '#FAF0E6',
-        '#FAF3F0', '#FBEEE6', '#FCE4EC', '#FDEBD0'
-      ];
+
+
+const taskContainer = document.querySelector('#task-container')
+const addNewTaskInputBtn = document.querySelector('#add-new-task-input')
+const addTaskInput = (e) => {
+    e.preventDefault()
+    divEl = createTaskInput()
+    taskContainer.append(divEl)
+}
+addNewTaskInputBtn.addEventListener('click', (e) =>addTaskInput(e))
+
+const createTaskInput = (task) => {
+    let divEl = document.createElement('div')
+        divEl.classList.add('flex','items-center','gap-2.5')
+    let inputEl = document.createElement('input')
+        inputEl.classList.add('bg-[#2c2c38]','border-2','border-[#353541]','text-gray-900','text-sm','rounded-lg','w-full','p-2.5','placeholder-[#686872]','mb-3')
+        inputEl.setAttribute('type', 'text')
+        inputEl.setAttribute('name', 'subtask')
+        inputEl.value = task ? task.title : ''
+    let spanEl = document.createElement('span')
+        spanEl.classList.add('block')
+        spanEl.innerHTML = 
+        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="3" stroke="#828fa3" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>`
+    divEl.append(inputEl,spanEl)
+    return divEl
+}
+
+
+
+
+const getRandomVibrantColor = () => {
+    const vibrantColors = [
+        '#ff3f3f', '#FFA500', '#FFFF00', '#99cc99', '#008000', '#0000FF',
+        '#4B0082', '#EE82EE', '#FF69B4', '#8B008B', '#FFD700',
+        '#FF00FF', '#00FFFF','#ff9999', '#ffcc99', '#ffff99', '#99ccff',
+        '#9999e6', '#ffccff', '#ff99cc', '#ffffcc', '#ff99ff', '#99ffff'];
 
     // Generate a random index
-    const randomIndex = Math.floor(Math.random() * pastelColors.length);
+    const randomIndex = Math.floor(Math.random() * vibrantColors.length);
   
     // Return the random pastel color
-    return pastelColors[randomIndex];
+    return vibrantColors[randomIndex];
 }
 
-const form = document.getElementById('task-form')
-form.addEventListener('submit', (event) => {
+const taskForm = document.getElementById('task-form')
+taskForm.addEventListener('submit', (event) => {
     // Prevent the default action (refreshing the page)
     event.preventDefault();
 
     // Get the form data
-    const formData = new FormData(form);
-    board = getBoard(appData.currentBoard)
-    board.cols.map(col => {
+    const formData = new FormData(taskForm);
+    // console.log(formData.getAll('subtask').filter(task => task ? task : undefined))
+    let cols = getCols()
+    cols.map(col => {
         if (formData.get('status') == col.title){
             col.cards.push(
                 {
                 title: formData.get('title'),
                 description: formData.get('description'),
                 id: generateId('card'),
-                color: getRandomPastelColor(),
-                subtasks: formData.getAll('subtask').map(task => {
+                color: getRandomVibrantColor(),
+                subtasks: formData.getAll('subtask').filter(task => {
+                if(task) {
                     return  {
                             title: task,
                             isCompleted: false,
                             id: generateId('task')
                             }
+                        } else undefined
                     })
                 }
             )
@@ -271,5 +298,93 @@ form.addEventListener('submit', (event) => {
     })
     localStorage.setItem('app_data', JSON.stringify(appData));
     init()
-    form.reset();
+    taskForm.reset();
 });
+
+const addNewColumnContainer = document.querySelector('#add-column-container')
+const addNewColumnBtnBig = document.querySelector('#column-btn-big')
+const boardName = document.querySelector('#board-name')
+const columnContainer = document.querySelector('#column-container')
+
+
+const showAddNewColumn = () => {
+    addNewTaskBackground.removeAttribute('hidden')
+    addNewColumnContainer.removeAttribute('hidden')
+    let board = getBoard(appData.currentBoard)
+    boardName.value = board.title
+    columnContainer.innerHTML = ''
+    board.cols.map(col => {
+        divEl = createColumnInput(col)
+        columnContainer.append(divEl)
+    })
+}
+addNewColumnBtnBig.addEventListener('click', () =>showAddNewColumn())
+
+const addNewColumnInputBtn = document.querySelector('#add-new-column-input')
+const addColumnInput = (e) => {
+    e.preventDefault()
+    divEl = createColumnInput()
+    columnContainer.append(divEl)
+}
+addNewColumnInputBtn.addEventListener('click', (e) =>addColumnInput(e))
+
+const createColumnInput = (col) => {
+    let divEl = document.createElement('div')
+        divEl.classList.add('flex','items-center','gap-2.5')
+    let inputEl = document.createElement('input')
+        inputEl.classList.add('bg-[#2c2c38]','border-2','border-[#353541]','text-gray-900','text-sm','rounded-lg','w-full','p-2.5')
+        inputEl.setAttribute('type', 'text')
+        inputEl.setAttribute('name', 'column')
+        inputEl.setAttribute('required', true)
+        inputEl.value = col ? col.title : ''
+    let spanEl = document.createElement('span')
+        spanEl.classList.add('block')
+        spanEl.innerHTML = 
+        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="3" stroke="#828fa3" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>`
+    divEl.append(inputEl,spanEl)
+    return divEl
+}
+
+
+const columnForm = document.getElementById('column-form')
+columnForm.addEventListener('submit', (event) => {
+    // Prevent the default action (refreshing the page)
+    event.preventDefault();
+
+    // Get the form data
+    const formData = new FormData(columnForm);
+    // console.log(formData.getAll('subtask').filter(task => task ? task : undefined))
+    let board = getBoard(appData.currentBoard)
+    board.title = formData.get('board-name')
+    data = formData.getAll('column')
+    data.map((col,i) => {
+        if(board.cols[i]) {
+            board.cols[i].title = col
+        } else {
+            board.cols.push({
+                title: col,
+                id: generateId('column'),
+                color: getRandomVibrantColor(),
+                cards: []
+            })
+        }
+    })
+    localStorage.setItem('app_data', JSON.stringify(appData));
+    init()
+});
+
+
+const addNewTaskBackground = document.querySelector('#add-task-background')
+const hideAddNewTask = () => {
+    addNewTaskBackground.setAttribute('hidden',true)
+    addNewTaskContainer.setAttribute('hidden',true)
+    addNewColumnContainer.setAttribute('hidden',true)
+    taskForm.reset();
+}
+addNewTaskBackground.addEventListener('click', (e) => {
+    console.log(e)
+    if(e.target.id === addNewTaskBackground.id) hideAddNewTask();
+})
