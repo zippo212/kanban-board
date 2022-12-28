527e02dc396bfa8e7820711409551708d1536f4b
Oleg Maizer <zippooleg3@gmail.com>
2022-12-28 21:11:42 +0200

    Added sidebar style

--- script.js
+++ script.js
@@ -105,9 +105,13 @@ const fillData = (data) => {
         let boardSelection = document.createElement('div')
             boardSelection.setAttribute('id',board.id)
             boardSelection.setAttribute('onClick','changeBoard(id)')
-            boardSelection.classList.add(`${data.currentBoard === board.id ? 'bg-red-300' : 'bg-green-300'}`)
-            boardSelection.textContent = board.title
-            BoardSideBar.append(boardSelection)
+            boardSelection.className = `${data.currentBoard === board.id ? 'bg-[#635fc7] text-white fill-white' : 'bg-transparent text-[#828fa3] fill-[#828fa3]'} text-base font-semibold pl-5 py-3 rounded-r-full hover:bg-[#635fc71a] hover:text-[#635fc7] cursor-pointer gap-4 flex items-center hover:fill-[#635fc7]`
+            svgDiv = document.createElement('div')
+            svgDiv.innerHTML = '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z"></path></svg>'
+            spanEl = document.createElement('span')
+            spanEl.textContent = board.title
+            boardSelection.append(svgDiv,spanEl)
+                BoardSideBar.append(boardSelection)
     })
     let cols = getCols()
     // create column
@@ -241,6 +245,7 @@ taskForm.addEventListener('submit', (event) => {
     })
     localStorage.setItem('app_data', JSON.stringify(appData));
     init()
+    taskContainer.innerHTML = ''
     taskForm.reset();
 });
 
@@ -336,6 +341,7 @@ boardForm.addEventListener('submit', (event) => {
     )
     localStorage.setItem('app_data', JSON.stringify(appData));
     init()
+    newBoardColumnContainer.innerHTML = ''
     boardForm.reset()
 });
 
